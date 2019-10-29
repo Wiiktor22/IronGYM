58dd283f7eae864b6f6894076e5a367352af0a27
Wiktor Szlegier <22wiktors@gmail.com>
2019-10-29 16:10:23 +0100

    Create section "Training Plans" and licence.txt II

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 $('.burger, .close_burger').on('click', function(){
     $('nav').toggleClass('on');
-})
+});
+
+// SWIPER - TRAINING PLANS
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 20,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows : true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
